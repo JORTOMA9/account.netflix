744d94794f66f64bc51889d2765fd804578a38a5
JORTOMA9 <youssefrhazzal@icloud.com>
2026-06-04 15:43:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 import Account from './pages/Account';
 import NotFound from './pages/NotFound';
-
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+  const browserLang = navigator.language || navigator.userLanguage;
+
+  let targetLang = "en";
+
+  if (browserLang.startsWith("de")) {
+    targetLang = "de";
+  } else if (browserLang.startsWith("fr")) {
+    targetLang = "fr";
+  } else if (browserLang.startsWith("ar")) {
+    targetLang = "ar";
+  }
+
+  document.cookie = `googtrans=/en/${targetLang}; path=/`;
+}, []);
   return (
     <Router>
+      <div id="google_translate_element"></div>
       <Routes>    
         <Route path="/" element={<Navigate to="/login" replace />} />   
         <Route path='/login' element={<Login/>}/>
